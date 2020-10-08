3aa9096c9a58ef459b9ee668041f4d3b83e78202
Kefikhalil <naroqt@gmail.com>
2020-10-08 15:43:16 -0700

    updated

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,8 +3,3 @@ exec { 'Debugging':
   command => 'sed -i \'s/^holberton hard nofile.*/holberton hard nofile 64000/g\' /etc/security/limits.conf',
   path    => '/bin/'
 }
-#Debugging_4
-exec { 'Debugging':
-  command => 'sed -i \'s/^holberton soft nofile.*/holberton soft nofile 64000/g\' /etc/security/limits.conf',
-  path    => '/bin/',
-}
